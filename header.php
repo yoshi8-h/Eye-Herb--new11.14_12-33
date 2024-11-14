860c5510142f2e7f6f95ad0d9d745af3df1bae21
yoshi8-h <hirao.y.freelance@gmail.com>
2024-11-14 20:08:33 +0900

    ドロワーメニュー 作成途中

--- header.php
+++ header.php
@@ -75,97 +75,28 @@
 
   <!-- ドロワーメニュー -->
   <div class="drawer-menu js-drawer-menu">
-    <div class="drawer-menu__wrap">
-      <div class="drawer-menu__left">
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_post_type_archive_link('campaign')); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">キャンペーン</p>
-          </a>
-          <div class="drawer-link__details">
-            <?php
-            //　カスタム投稿タイプ『campaign』にカテゴリ(タクソノミー)が追加される度に、そのタクソノミーの一覧ページへのリンクが生成されるようにする。
-              // カスタムタクソノミーのカテゴリを取得
-              $terms = get_terms(array(
-                'taxonomy' => 'campaign_category',
-                'hide_empty' => false,
-              ));
-
-              // 取得したカテゴリが存在する場合、リンクを生成
-              if (!empty($terms) && !is_wp_error($terms)) {
-                foreach ($terms as $term) {
-                  // 各カテゴリのリンクを出力
-                  echo '<a href="' . esc_url(get_term_link($term)) . '" class="drawer-link__detail">' . esc_html($term->name) . '</a>';
-                }
-              }
-            ?>
+    <div class="drawer-menu__inner">
+      <div class="drawer-menu__wrap">
+        <div class="drawer-menu__top title01">
+          <p class="title01__text">代官山に佇む癒しのプライベートサロン</p>
+          <div class="title01__img"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/title01-img-sp.webp" alt="Eye Herb"></div>
+        </div>
+        <div class="drawer-menu__nav nav">
+          <a href="<?php echo esc_url(get_permalink(get_page_by_path('about'))); ?>" class="nav__item">About Eye Herb</a>
+          <a href="<?php echo esc_url(get_permalink(get_page_by_path('menu'))); ?>" class="nav__item">Menu</a>
+          <a href="<?php echo esc_url(get_permalink(get_page_by_path('before-after'))); ?>" class="nav__item">Before/After</a>
+          <a href="<?php echo esc_url(get_permalink(get_page_by_path('recruit'))); ?>" class="nav__item">Recruit</a>
+          <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="nav__item">Contact</a>
+        </div>
+        <div class="drawer-menu__bottom">
+          <div class="drawer-menu__info info">
+            <address class="info__address">東京都渋谷区代官山町2-5 コレタス代官山4F</address>
+            <div class="info__tel">【TEL】03-6416-4229 (12:00 - 19:00)</div>
           </div>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('about-us'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">私たちについて</p>
-          </a>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('information'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">ダイビング情報</p>
-          </a>
-          <div class="drawer-link__details">
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('information'))); ?>?category=category1" class="drawer-link__detail">ライセンス講習</a>
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('information'))); ?>?category=category3" class="drawer-link__detail">体験ダイビング</a>
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('information'))); ?>?category=category2" class="drawer-link__detail">ファンダイビング</a>
+          <div class="drawer-menu__sns-wrap sns-wrap">
+            <a href="" class="sns-wrap__insta"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/instagram-icon.webp" alt="instagram"></a>
+            <a href="" class="sns-wrap__line"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/line-icon.webp" alt="LINE"></a>
           </div>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('blog'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">ブログ</p>
-          </a>
-        </div>
-      </div>
-      <div class="drawer-menu__right">
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_post_type_archive_link('voice')); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">お客様の声</p>
-          </a>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('price'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">料金一覧</p>
-          </a>
-          <div class="drawer-link__details">
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('price'))); ?>#price-page__license" class="drawer-link__detail">ライセンス講習</a>
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('price'))); ?>#price-page__experience" class="drawer-link__detail">体験ダイビング</a>
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('price'))); ?>#price-page__fun" class="drawer-link__detail">ファンダイビング</a>
-          </div>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('faq'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">よくある質問</p>
-          </a>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('privacypolicy'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">プライバシー<br>ポリシー</p>
-          </a>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('terms-of-service'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">利用規約</p>
-          </a>
-        </div>
-        <div class="drawer-menu__link drawer-link">
-          <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="drawer-link__main">
-            <div class="drawer-link__icon"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/starfish.webp" alt="ヒトデのアイコン"></div>
-            <p class="drawer-link__text">お問い合わせ</p>
-          </a>
         </div>
       </div>
     </div>
