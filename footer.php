18356076eec48818ad610b0b1506b5006183fc51
yoshi8-h <hirao.y.freelance@gmail.com>
2024-11-14 15:24:49 +0900

    WordPress構築できるようになった段階時

---
+++ footer.php
@@ -0,0 +1,14 @@
+  <!-- TOPへ戻るボタン -->
+  <a href="#" class="top-btn js-top-btn">
+  </a>
+
+  <!-- footer -->
+  <footer class="footer l-footer <?php echo is_404() ? 'l-footer--404' : ''; ?>">
+    <div class="footer__inner inner">
+      footer
+    </div>
+  </footer>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+  <main class="main">
+    front-page
+  </main>
+
+<?php get_footer(); ?>
