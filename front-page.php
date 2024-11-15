1aefe2dbece9c0d8532032a889cccbfa11821b26
yoshi8-h <hirao.y.freelance@gmail.com>
2024-11-15 15:48:56 +0900

    fv PC時完成し、SP時を作成中

--- front-page.php
+++ front-page.php
@@ -12,6 +12,30 @@
         </div>
       </div>
       <div class="fv__logo"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/fv-logo.webp" alt="Eye Herb"></div>
+      <!-- (円形)予約ボタン -->
+      <a href="" class="fv__reserve-circle reserve-circle">
+        <div class="reserve-circle__brown">
+          <div class="reserve-circle__en">Reserve</div>
+          <div class="reserve-circle__ja">予約する</div>
+          <div class="reserve-circle__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="37" viewBox="0 0 34 37" fill="none">
+            <g clip-path="url(#clip0_1_2526)">
+              <path d="M12.7479 13.6035H14.8345V15.8506H12.7479V13.6035ZM19.0076 27.0859H21.0942V29.333H19.0076V27.0859ZM25.2673 13.6035H27.3539V15.8506H25.2673V13.6035ZM19.0076 13.6035H21.0942V15.8506H19.0076V13.6035ZM12.7479 18.0977H14.8345V20.3447H12.7479V18.0977ZM6.48821 18.0977H8.57478V20.3447H6.48821V18.0977ZM25.2673 18.0977H27.3539V20.3447H25.2673V18.0977ZM19.0076 18.0977H21.0942V20.3447H19.0076V18.0977ZM12.7479 22.5918H14.8345V24.8389H12.7479V22.5918ZM6.48821 22.5918H8.57478V24.8389H6.48821V22.5918ZM25.2673 22.5918H27.3539V24.8389H25.2673V22.5918ZM19.0076 22.5918H21.0942V24.8389H19.0076V22.5918ZM12.7479 27.0859H14.8345V29.333H12.7479V27.0859ZM6.48821 27.0859H8.57478V29.333H6.48821V27.0859ZM33.6136 2.36816V33.8271H0.228516V2.36816H6.48821V0.121094H8.57478V2.36816H25.2673V0.121094H27.3539V2.36816H33.6136ZM2.31508 4.61523V9.10938H31.527V4.61523H27.3539V6.8623H25.2673V4.61523H8.57478V6.8623H6.48821V4.61523H2.31508ZM31.527 31.5801V11.3564H2.31508V31.5801H31.527Z" fill="white"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_1_2526">
+                <rect width="33.385" height="35.9531" fill="white" transform="translate(0.228516 0.121094)"/>
+              </clipPath>
+            </defs>
+          </svg>
+          </div>
+        </div>
+      </a>
+      <!-- scroll-bar -->
+      <div class="fv__scroll-bar scroll-bar">
+        <div class="scroll-bar__text">Scroll Down</div>
+        <div class="scroll-bar__bar"></div>
+			</div>
     </div>
 
   </main>
