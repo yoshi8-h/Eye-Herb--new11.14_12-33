21e8519ed2cb038fc8b8553ffc07134816e2f0c7
yoshi8-h <hirao.y.freelance@gmail.com>
2024-11-18 00:03:49 +0900

    page-about.php 作成開始時

--- page-about.php
+++ page-about.php
@@ -1,1 +1,36 @@
-aboutページ
+<?php get_header(); ?>
+
+  <main class="main">
+    <!-- fv2 -->
+    <div class="fv2"></div>
+
+    
+
+    <!-- access セクション -->
+    <section class="access l-access">
+      <div class="access__inner inner">
+        <div class="access__wrap">
+          <div class="access__left">
+            <h2 class="access__title section-title01 section-title01--none-bar">Access</h2>
+            <p class="access__info">
+              Eye Herb Beauty Salon<br
+              >東京都 渋谷区 代官山町2-5 コレタス代官山4F<br
+              >【営業時間】12:00 -19:00(最終受付19:00)
+            </p>
+            <p class="access__access">
+              恵比寿駅 徒歩12分<br
+              >東急東横線 / 代官山駅 徒歩約7分<br
+              >JR山手線 / 渋谷駅 徒歩約7分<br>JR埼京線 / 渋谷駅 徒歩約7分<br
+              >JR湘南新宿ライン / 渋谷駅 徒歩約7分
+            </p>
+          </div>
+          <div class="access__right">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.987943746225!2d139.70243747602382!3d35.65266867259646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b5b36f4ada1%3A0x4585b16474b38f91!2z44CSMTUwLTAwMzQg5p2x5Lqs6YO95riL6LC35Yy65Luj5a6Y5bGx55S677yS4oiS77yVIOOCs-ODrOOCv-OCueS7o-WumOWxsSA0Zg!5e0!3m2!1sja!2sjp!4v1731834145436!5m2!1sja!2sjp" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+<?php get_footer(); ?>
